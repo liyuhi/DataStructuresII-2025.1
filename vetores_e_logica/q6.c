c7a66f4de7af483bcf93299ea74bdc00cdd347fe
liyuhi <whorshes@gmail.com>
2025-05-07 23:31:55 -0300

    Adicionando q6

--- vetores_e_logica/q6.c
+++ vetores_e_logica/q6.c
@@ -2,10 +2,45 @@
 
 Crie duas matrizes 3x3, some seus valores e exiba o resultado.*/
 
-int main(){
+#include <stdio.h>
 
+int main() {
+    int matriz1[3][3];
+    int matriz2[3][3];
+    int resultado[3][3];
+    int i, j;
 
+    // Leitura da primeira matriz
+    printf("Digite os valores da primeira matriz 3x3:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
 
+    // Leitura da segunda matriz
+    printf("Digite os valores da segunda matriz 3x3:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    // Soma das matrizes
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            resultado[i][j] = matriz1[i][j] + matriz2[i][j];
+        }
+    }
+
+    // Exibe o resultado
+    printf("Matriz resultante da soma:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", resultado[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
