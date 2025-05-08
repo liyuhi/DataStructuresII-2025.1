8b07f037caf9efa84ebb69bd7ac004dec50db585
liyuhi <whorshes@gmail.com>
2025-05-07 23:51:23 -0300

    Adicionando q13

--- algoritmoslogicos/q13.c
+++ algoritmoslogicos/q13.c
@@ -2,10 +2,25 @@
 
 Crie uma matriz 4x4 e calcule a soma da diagonal principal.*/
 
-int main(){
+#include<stdio.h>
 
+int main() {
+    int matriz[4][4];
+    int i, j;
+    int soma = 0;
 
+    printf("Digite os valores da matriz 4x4:\n");
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
 
+    for (i = 0; i < 4; i++) {
+        soma += matriz[i][i];  // soma elementos da diagonal principal
+    }
+
+    printf("Soma da diagonal principal: %d\n", soma);
 
     return 0;
 }
