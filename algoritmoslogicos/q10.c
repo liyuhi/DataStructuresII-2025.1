fb68cd5dfeab23d5dc833074cbaef9bcb0b07012
liyuhi <whorshes@gmail.com>
2025-05-08 00:19:16 -0300

    Adicionando comentários à q10

--- algoritmoslogicos/q10.c
+++ algoritmoslogicos/q10.c
@@ -11,19 +11,23 @@ int fibonacci(int n) {
     } else if (n == 1) {
         return 1; // Caso base: F(1) = 1
     } else {
-        return fibonacci(n - 1) + fibonacci(n - 2); // Recursão
+        // Recursão: F(n) = F(n-1) + F(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
 
 int main() {
     int n;
 
+    // Solicita ao usuário o valor de n
     printf("Digite o valor de n para calcular o n-ésimo número de Fibonacci: ");
     scanf("%d", &n);
 
+    // Verifica se o número é não negativo
     if (n < 0) {
         printf("Por favor, insira um número não negativo.\n");
     } else {
+        // Chama a função fibonacci e exibe o resultado
         printf("O %d-ésimo número de Fibonacci é: %d\n", n, fibonacci(n));
     }
 
