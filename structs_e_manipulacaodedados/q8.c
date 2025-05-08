905762a69fb84f5ce2232a5309c39c2cb3d40efb
liyuhi <whorshes@gmail.com>
2025-05-08 00:15:25 -0300

    Adicionando comentários à q8

--- structs_e_manipulacaodedados/q8.c
+++ structs_e_manipulacaodedados/q8.c
@@ -2,17 +2,20 @@
 
 Crie uma struct Produto com nome, código, quantidade e preço. Permita cadastrar, buscar e listar produtos*/
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-#define max_nome 100
-#define max_produtos 100
+// Tamanho máximo para o nome do produto
+#define max_nome 100    
+ // Número máximo de produtos que podem ser cadastrados
+#define max_produtos 100  
 
+// Definição da estrutura Produto
 typedef struct {
-    char nome[max_nome];
-    int codigo;
-    int quantidade;
-    float preco;
+    char nome[max_nome];  // Nome do produto
+    int codigo;           // Código do produto
+    int quantidade;       // Quantidade em estoque
+    float preco;          // Preço do produto
 } Produto;
 
 // Função para cadastrar um novo produto
@@ -24,9 +27,9 @@ void cadastrarProduto(Produto produtos[], int *num_produtos) {
 
     printf("Cadastro do produto %d:\n", *num_produtos + 1);
     printf("Nome: ");
-    getchar(); // Limpar buffer
+    getchar(); // Limpa o buffer para a leitura correta do fgets
     fgets(produtos[*num_produtos].nome, max_nome, stdin);
-    produtos[*num_produtos].nome[strcspn(produtos[*num_produtos].nome, "\n")] = 0; // Remove '\n'
+    produtos[*num_produtos].nome[strcspn(produtos[*num_produtos].nome, "\n")] = 0; // Remove '\n' da string
 
     printf("Código: ");
     scanf("%d", &produtos[*num_produtos].codigo);
@@ -41,7 +44,7 @@ void cadastrarProduto(Produto produtos[], int *num_produtos) {
     printf("Produto cadastrado com sucesso!\n\n");
 }
 
-// Função para buscar um produto por código
+// Função para buscar um produto pelo código
 void buscarProduto(Produto produtos[], int num_produtos) {
     int codigo;
     printf("Digite o código do produto para buscar: ");
@@ -60,7 +63,7 @@ void buscarProduto(Produto produtos[], int num_produtos) {
     printf("Produto com código %d não encontrado.\n\n", codigo);
 }
 
-// Função para listar todos os produtos
+// Função para listar todos os produtos cadastrados
 void listarProdutos(Produto produtos[], int num_produtos) {
     if (num_produtos == 0) {
         printf("Nenhum produto cadastrado.\n\n");
@@ -78,9 +81,9 @@ void listarProdutos(Produto produtos[], int num_produtos) {
 }
 
 int main() {
-    Produto produtos[max_produtos];
-    int num_produtos = 0;
-    int opcao;
+    Produto produtos[max_produtos];   // Array para armazenar os produtos
+    int num_produtos = 0;             // Contador de produtos cadastrados
+    int opcao;                       // Opção do menu
 
     do {
         printf("Menu:\n");
@@ -93,13 +96,13 @@ int main() {
 
         switch (opcao) {
             case 1:
-                cadastrarProduto(produtos, &num_produtos);
+                cadastrarProduto(produtos, &num_produtos);  // Cadastra um novo produto
                 break;
             case 2:
-                buscarProduto(produtos, num_produtos);
+                buscarProduto(produtos, num_produtos);      // Busca um produto pelo código
                 break;
             case 3:
-                listarProdutos(produtos, num_produtos);
+                listarProdutos(produtos, num_produtos);     // Lista todos os produtos cadastrados
                 break;
             case 4:
                 printf("Saindo do programa...\n");
