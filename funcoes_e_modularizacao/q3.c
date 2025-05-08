1965662d869cf83635608884e81b2f8d084c86d8
liyuhi <whorshes@gmail.com>
2025-05-07 23:12:43 -0300

    Respondendo q3

--- funcoes_e_modularizacao/q3.c
+++ funcoes_e_modularizacao/q3.c
@@ -2,7 +2,30 @@
 
 Escreva uma função que inverte os dígitos de um número (ex: 1234 → 4321).*/
 
-int main(){
+#include<stdio.h>
+
+int inverterNumero(int numero) {
+    int invertido = 0;
+
+    while (numero != 0) {
+        invertido = invertido * 10 + numero % 10;
+        numero /= 10;
+    }
+
+    return invertido;
+}
+
+int main() {
+    int numero;
+
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    int resultado = inverterNumero(numero);
+    printf("Número invertido: %d\n", resultado);
+
+    return 0;
+}
 
 
 
