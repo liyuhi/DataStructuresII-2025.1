53e994a0e5bf06d12f5695a7228baf77d979f4cc
liyuhi <whorshes@gmail.com>
2025-05-08 00:11:32 -0300

    Adicionando comentários à q6

--- vetores_e_logica/q6.c
+++ vetores_e_logica/q6.c
@@ -2,45 +2,45 @@
 
 Crie duas matrizes 3x3, some seus valores e exiba o resultado.*/
 
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int matriz1[3][3];
-    int matriz2[3][3];
-    int resultado[3][3];
-    int i, j;
+    int matriz1[3][3]; // Declaração da primeira matriz 3x3
+    int matriz2[3][3]; // Declaração da segunda matriz 3x3
+    int resultado[3][3]; // Declaração da matriz para armazenar o resultado da soma
+    int i, j; // Variáveis de controle para os loops
 
     // Leitura da primeira matriz
     printf("Digite os valores da primeira matriz 3x3:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &matriz1[i][j]);
+    for (i = 0; i < 3; i++) { // Loop para percorrer as linhas
+        for (j = 0; j < 3; j++) { // Loop para percorrer as colunas
+            scanf("%d", &matriz1[i][j]); // Lê o valor e armazena na matriz1
         }
     }
 
     // Leitura da segunda matriz
     printf("Digite os valores da segunda matriz 3x3:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &matriz2[i][j]);
+    for (i = 0; i < 3; i++) { // Loop para percorrer as linhas
+        for (j = 0; j < 3; j++) { // Loop para percorrer as colunas
+            scanf("%d", &matriz2[i][j]); // Lê o valor e armazena na matriz2
         }
     }
 
     // Soma das matrizes
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            resultado[i][j] = matriz1[i][j] + matriz2[i][j];
+    for (i = 0; i < 3; i++) { // Loop para percorrer as linhas
+        for (j = 0; j < 3; j++) { // Loop para percorrer as colunas
+            resultado[i][j] = matriz1[i][j] + matriz2[i][j]; // Soma os elementos correspondentes
         }
     }
 
     // Exibe o resultado
     printf("Matriz resultante da soma:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%4d", resultado[i][j]);
+    for (i = 0; i < 3; i++) { // Loop para percorrer as linhas
+        for (j = 0; j < 3; j++) { // Loop para percorrer as colunas
+            printf("%4d", resultado[i][j]); // Imprime o elemento da matriz resultante
         }
-        printf("\n");
+        printf("\n"); // Nova linha após imprimir cada linha da matriz
     }
 
-    return 0;
+    return 0; 
 }
