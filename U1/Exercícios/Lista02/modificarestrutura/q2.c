eeb83b0173d0e3b9866d12e785a3f74d9fc97881
liyuhi <whorshes@gmail.com>
2025-06-06 13:45:35 -0300

    Delete U1/Exercícios/Lista02/modificarestrutura/q2.c

--- U1/Exercícios/Lista02/modificarestrutura/q2.c
+++
@@ -1,9 +0,0 @@
-//Implemente a estrutura de dados para a árvore binária de busca.
-
-typedef struct Aluno {
-    char nome[50];           
-    int matricula;          
-    float nota;              
-    struct Aluno *esquerdo;  
-    struct Aluno *direito;  
-} Aluno;
